ee540869ef4cfc2e9228c0dbebd3707ed6bb839b
Not-a-whale <49877969+Not-a-whale@users.noreply.github.com>
2022-06-24 21:42:29 +0200

    hw 11 completed

---
+++ HW10/Classes/FirstClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW10.Classes
+{
+    delegate void SomeDelegate(bool arg);
+    delegate int PowDeleg(int a, int b);
+    internal class FirstClass
+    {
+        public static PowDeleg powDeleg = Pow;
+        public static int Pow(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public static void Show(bool arg) => Console.WriteLine(arg ? "Can be divided" : "Is not divisible");
+    }
+}
---
+++ HW10/Classes/SecondClass.cs
@@ -0,0 +1,19 @@
+﻿namespace HW10.Classes
+{
+    delegate bool isDivisible(int num);
+    internal class SecondClass
+    {
+        private static int PowResult { get; set; }
+        public static isDivisible Calc(PowDeleg deleg, int a, int b)
+        {
+            PowResult = deleg(a, b);
+            isDivisible res = Result;
+            return res;
+        }
+
+        public static bool Result(int num)
+        {
+            return PowResult % num == 0;
+        }
+    }
+}
